fb1d7ed8e834dd20b21d8701b79aaf46044236a9
Federico Binda <federico.binda@physics.uu.se>
2014-12-03 15:59:22 +0100

    am_i_inside added, check if the snake is inside the screen

--- snake.py
+++ snake.py
@@ -4,8 +4,17 @@ import time
 """A small snake game"""
 
 mydirection = 1 # 0 = up, 1 = right, 2 = down, 3 = left
-myposition = [1,1]
+myposition = [1,1] # y,x
 mylength = 5 # length of the snake
+
+def am_i_inside(height,width):
+	"""check if the snake hit the wall"""
+	if myposition[0] < 1 or myposition[0] >= height:
+		return False
+	elif myposition[1] < 1 or myposition[1] >= width:
+		return False
+	else:
+		return True
 
 def mycurse(stdscr):
 	global myposition
@@ -15,11 +24,12 @@ def mycurse(stdscr):
 	height = 10
 	width = 40
 	win = curses.newwin(height, width, begin_y, begin_x) # init window
+	win.nodelay(1) # getch does not block the program
 	myposition = [height/2, width/2] # start snake in the middle
-	move(win)
-	move(win)
-	move(win)
-	time.sleep(5)
+	key = ''
+	while key != ord('q') and am_i_inside(height,width):
+		key = win.getch()
+		move(win)
 	
 def move(win,dt=1):
 	"""move the snake"""
@@ -29,7 +39,6 @@ def move(win,dt=1):
 	win.addstr(myposition[0],myposition[1] - mylength,'x'*mylength) # draw snake
 	win.refresh()
 	time.sleep(dt)
-
 	
 if __name__ == '__main__':
 	curses.wrapper(mycurse)
